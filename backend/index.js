8e77d0ae2f8c3bc9844dda35aa5ef4a54db58a88
Gurjap Singh <gurjapsingh@Gurjaps-MacBook-Air-2.local>
2022-03-20 14:59:19 -0400

    changed API port and added gitignore

--- backend/index.js
+++ backend/index.js
@@ -16,4 +16,4 @@ app.get('/', (req, res) => {
   })
 });
 
-app.listen(3000, () => console.log('Example app is listening on port 3000.'));
+app.listen(3001, () => console.log('Example app is listening on port 3001'));
